7dc39bacdd5b0322991a7d31eddc735a9dae2c41
lga <18270234907@163.com>
2020-10-03 17:43:53 +0800

    semaphore

---
+++ src/main/java/com/lga/juc/SemaphoreTest.java
@@ -0,0 +1,31 @@
+package com.lga.juc;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreTest {
+
+    /**
+     * 限流的时候使用
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 1; i <= 6; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "抢到车位");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(Thread.currentThread().getName() + "离开车位车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
